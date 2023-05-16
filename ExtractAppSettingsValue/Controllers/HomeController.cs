1c3a0a1d0244749d85edbf48d94e978007182fc1
Md Farid Uddin <v-mdud@microsoft.com>
2023-05-16 09:50:18 +0800

    Add project files.

---
+++ ExtractAppSettingsValue/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ExtractAppSettingsValue.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExtractAppSettingsValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _appSettingsValue;
+        private string clusterSettings;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _appSettingsValue = configuration;
+            clusterSettings = _appSettingsValue["ClusterSettings:Host"]!;
+        }
+
+        public IActionResult Index()
+        {
+            var getAppSettingsValue = clusterSettings;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
